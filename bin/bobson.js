087eb0712124b598cdecf87caff39c2d9a68053b
Christopher Hiller <boneskull@boneskull.com>
2017-08-11 13:49:50 -0700

    add "inverse" mutation; add --dry-run; use @std/esm

--- bin/bobson.js
+++ bin/bobson.js
@@ -1,27 +1,8 @@
 #!/usr/bin/env node
+
+/* eslint-disable strict */
 'use strict';
 
-const {Names, Name} = require('../lib/names');
-const Twit = require('twit');
-const config = require('../lib/config');
+require('@std/esm');
 
-const firstnames = new Names('firstnames');
-const surnames = new Names('surnames');
-
-(async function () {
-  Promise.all([
-    firstnames.pick(),
-    surnames.pick()
-  ])
-    .then(([firstname, surname]) => {
-      const name = new Name(firstname, surname);
-      const status = String(name.mutate());
-      console.log(status);
-
-      const T = new Twit(config);
-      return T.post('statuses/update', {status});
-    })
-    .then(() => {
-      console.error('OK');
-    });
-}());
+require('../lib/cli');
